71b9c7926d16e90a1e8d4c93dae1f222f585c2da
babyboy51 <glosmork@gmail.com>
2020-05-17 12:21:23 +0900

    task2 done

---
+++ task2/index.php
@@ -0,0 +1,37 @@
+<?php $a=3;
+$b+=7;
+echo $a+$b;?>
+
+<?php $array_month=["january","february","March","April","May","June","July","August","September",
+"October","November","Decemder"];
+echo $array_month[7];?>
+
+
+<?php $hello="Hello,";
+$name.="babyboy";
+$world.="'s World!";
+echo $hello;
+echo $name;
+echo $world;?>
+
+
+<?php $tech_boost="tech";
+$tech_boost.="_boost";
+echo $tech_boost;?>
+
+
+<?php $calendar_2018 =[
+ "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+ ];
+ echo $calendar_2018[December];?>
