4ce596bb30c8049dcd7fcf595b12331e2f5b26d0
babyboy51 <glosmork@gmail.com>
2020-05-24 12:14:51 +0900

    task3 終了

--- task3/index.php
+++ task3/index.php
@@ -1,1 +1,30 @@
-abcde
+<?php
+// 1
+ $name = Misaki;
+ if($name = Misaki){
+   echo "あなたの名前ではありません";
+ } else if($name = Mizuki){
+   echo "あなたの名前ではありません";
+ } else{
+   echo "私はあなたの名前です";
+ }
+
+// 2
+for($i =1; $i<10000;$i++){
+  echo $i;
+}
+
+// 3
+$fruits = array("apple","orange","melon","lemon","banana");
+foreach ($fruits as $fruits){
+  echo "要素は" . $fruits;
+  echo "\n";
+}
+// 4
+$start = 1;
+$end = 100;
+for($i = $start;$i < $end; $i++){
+if($i % 5== 0){
+echo $i;
+  }
+}
